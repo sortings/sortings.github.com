4ccd663911650f212f19e14b70bfb36c72f0d6f0
Hismatullin Kamil Damirovich <kamil_hism@mail.ru>
2013-03-23 19:05:34 +0400

    jquery lib and logic for code tabs

---
+++ javascripts/main.js
@@ -0,0 +1,19 @@
+jQuery(
+  function($) {
+    $(".tabnav-tabs li").on('click', function(e) {
+      if($(this).attr('class').split(' ').length != 2)
+      {
+        $selected = $(this).siblings('.selected');
+        $selected.removeClass('selected');
+
+        $code = $(this).closest('div').next('.code');
+        $whatToHide = $code.find('.' + $selected.attr('class').split('-')[0]);
+        $whatToShow = $code.find('.' + $(this).attr('class').split('-')[0]);
+
+        $(this).addClass('selected');
+
+        $whatToHide.first().hide();
+        $whatToShow.first().show();
+      }
+  });
+});
