54728af78c8c8b5dc1340d57ecdeb2f799ad1c96
Hismatullin Kamil Damirovich <kamil_hism@mail.ru>
2013-04-14 01:09:34 +0400

    merge sort

--- javascripts/sort_types.js
+++ javascripts/sort_types.js
@@ -23,4 +23,50 @@ Sort.bubble_recursive = function(data, sortParams)
   }
   isReady(sortParams);
 };
-/* */
+/* слиянием */
+Sort.merge_recursive = function(items, sortParams) {
+  Sort.merge_recursive_help(items);
+  isReady(sortParams);
+}
+Sort.merge_recursive_help = function(items) {
+  if (items.length == 1) {
+    return items;
+  }
+
+  var middle = Math.floor(items.length / 2),
+    left = items.slice(0, middle),
+    right = items.slice(middle);
+
+  return Sort.merge_help(Sort.merge_recursive_help(left), Sort.merge_recursive_help(right));
+}
+Sort.merge_help = function(left, right) {
+  var result = [];
+
+  while (left.length > 0 && right.length > 0) {
+    if (left[0] < right[0]) {
+        result.push(left.shift());
+    }
+    else {
+        result.push(right.shift());
+    }
+  }
+  return result.concat(left).concat(right);
+}
+Sort.merge = function(items, sortParams) {
+  if (items.length == 1) {
+    return items;
+  }
+  var work = [];
+  for (var i=0, len=items.length; i < len; i++) {
+      work.push([items[i]]);
+  }
+  work.push([]);
+
+  for (var lim=len; lim > 1; lim = Math.floor((lim+1)/2)) {
+    for (var j=0,k=0; k < lim; j++, k+=2) {
+        work[j] = Sort.merge_help(work[k], work[k+1]);
+    }
+    work[j] = [];
+  }
+  isReady(sortParams);
+}
